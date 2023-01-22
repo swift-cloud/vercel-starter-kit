fef02ce67b20f8bf3e78572ea5c6003a7389d718
Andrew Barba <barba@hey.com>
2023-01-22 17:04:49 -0500

    Update EdgeConfig

--- Sources/App/App.swift
+++ Sources/App/App.swift
@@ -4,7 +4,7 @@ import Vercel
 struct App: RequestHandler {
 
     func onRequest(_ req: Request, context: Context) async throws -> Response {
-        let greeting = EdgeConfig.default["greeting", default: "Swift"]
+        let greeting = EdgeConfig.default.get("greeting").string ?? "World"
         return .status(.ok).send("Hello, \(greeting)")
     }
 }
