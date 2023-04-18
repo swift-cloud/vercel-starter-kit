c9ec06d7d430b6a177e2f32f75d70ec2c1ce45e0
Andrew Barba <barba@hey.com>
2023-04-18 13:45:22 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.8
 
 import PackageDescription
 
@@ -8,7 +8,7 @@ let package = Package(
         .macOS(.v12)
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-cloud/Vercel", from: "1.7.1")
+        .package(url: "https://github.com/swift-cloud/Vercel", from: "1.8.0")
     ],
     targets: [
         .executableTarget(name: "App", dependencies: ["Vercel"])
