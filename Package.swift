ff82815353dc54be29030af76ca0455dcef029fb
Andrew Barba <barba@hey.com>
2023-05-01 13:29:51 -0400

    Implement KV store

--- Package.swift
+++ Package.swift
@@ -8,9 +8,10 @@ let package = Package(
         .macOS(.v12)
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-cloud/Vercel", from: "1.9.0")
+        .package(url: "https://github.com/swift-cloud/Vercel", from: "1.10.0"),
+        .package(url: "https://github.com/swift-cloud/Upstash", from: "1.0.1")
     ],
     targets: [
-        .executableTarget(name: "App", dependencies: ["Vercel"])
+        .executableTarget(name: "App", dependencies: ["Vercel", "Upstash"])
     ]
 )
--- Sources/App/App.swift
+++ Sources/App/App.swift
@@ -1,9 +1,16 @@
 import Vercel
+import Upstash
+
+let kv = RedisClient(
+    hostname: Vercel.Environment["KV_REST_API_URL"]!,
+    token: Vercel.Environment["KV_REST_API_TOKEN"]!
+)
 
 @main
 struct App: RequestHandler {
 
     func onRequest(_ req: Request, context: Context) async throws -> Response {
-        return .status(.ok).send("Hello, Swift")
+        let visits: Int = try await kv.exec("incr", ["visits"])
+        return .status(.ok).send("Hello, Swift. Visits \(visits)")
     }
 }
