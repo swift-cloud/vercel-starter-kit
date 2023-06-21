4cedc202afef8513104d5b9b6b0c6508b66f101d
Andrew Barba <barba@hey.com>
2023-06-21 16:29:04 -0400

    Deps

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .macOS(.v12)
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-cloud/Vercel", from: "1.11.0")
+        .package(url: "https://github.com/swift-cloud/Vercel", from: "1.13.0")
     ],
     targets: [
         .executableTarget(name: "App", dependencies: ["Vercel"])
