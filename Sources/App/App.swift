9044bf99c9f51c1d52733694c966b2df8e0ac3be
Andrew Barba <barba@hey.com>
2023-08-23 09:32:56 -0400

    Simplify vapor handler

--- Sources/App/App.swift
+++ Sources/App/App.swift
@@ -5,19 +5,11 @@ import VercelVapor
 struct App: VaporHandler {
 
     static let app: Application = {
-        // Create application
         let app = Application()
 
-        // Register basic route
-        app.get { req in
-            return Response(body: "Hello, Vapor")
+        app.get { _ in
+            "Hello, Vapor"
         }
-
-        // Register vercel handler
-        app.servers.use(.vercel)
-
-        // Start the app
-        try? app.start()
 
         return app
     }()
